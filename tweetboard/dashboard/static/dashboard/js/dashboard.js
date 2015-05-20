899c8f573c514757f3e4cbfb601821f09ddf9101
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2015-05-19 19:20:26 -0700

    AJAX proof of concept with initialization api.

--- tweetboard/dashboard/static/dashboard/js/dashboard.js
+++ tweetboard/dashboard/static/dashboard/js/dashboard.js
@@ -1,3 +1,14 @@
 externalFunction = function() {
-  alert('howdy, pardner!');
+  var ajax = new XMLHttpRequest();
+  ajax.open("GET", '/api/init/', false);
+  ajax.onreadystatechange = function() {
+    if (ajax.readyState == 4 && ajax.status == 200) {
+      var response = JSON.parse(ajax.responseText);
+      alert('Hello, ' + response.userName + ' (@' + response.twitterHandle + ')');
+      var img = document.createElement("img");
+      img.src = response.userPic;
+      document.body.appendChild(img)
+    }
+  }
+  ajax.send(null);
 };
