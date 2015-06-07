271922bf4a4fd60f8282c8d61fd67d73ead8da91
Eric Iwashita <ericiwashita@DN0a2333de.SUNet>
2015-06-06 17:22:02 -0700

    Stashing changes before pulling master.

--- tweetboard/api/urls.py
+++ tweetboard/api/urls.py
@@ -5,7 +5,6 @@ from api.views import *
 
 urlpatterns = [
     url(r'init', init),
-    url(r'sample_users', sample_users),
     url(r'get_tweets_by_user_id', get_tweets_by_user_id),
     url(r'get_users_retweet_by_original_user', get_users_retweet_by_original_user),
     url(r'get_user_by_id', get_user_by_id),
--- tweetboard/dashboard/static/dashboard/js/user_data.js
+++ tweetboard/dashboard/static/dashboard/js/user_data.js
@@ -11,6 +11,7 @@ d3.json("/api/get_user_by_id?user_id="+user_id, function(data) {
 	document.getElementById("profile_name").innerHTML = name;
 	document.getElementById("user_handle").innerHTML = screen_name;
 	document.getElementById("user_location").innerHTML = data.user.location;
+	document.getElementById("profile_pic").src = data.user.profile_image_url;
 
 	document.getElementById("home_wrapper").setAttribute("href", "/dashboard?user="+screen_name);
 	
